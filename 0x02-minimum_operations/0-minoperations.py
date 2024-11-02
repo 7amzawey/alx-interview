cf1364cf09e76213e5f594247f823c579d45d96f
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-02 10:44:52 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,23 +2,18 @@
 """Calculate the fewest number of operations needed to result in exactly n."""
 
 
-def largest_prime_number(n):
-    """Calculate the largest_prime_number."""
-    factor = 2
-    last_prime = None
-
-    while n > 1:
-        if n % factor == 0:
-            last_prime = factor
-            n //= factor
-        else:
-            factor += 1
-    return last_prime
-
-
 def minOperations(n):
     """Calculate the fewest number of operations."""
-    largest_pn = largest_prime_number(n)
-    the_div = n / largest_pn
+    if n < 2:
+        return 0
 
-    return int(largest_pn + the_div)
+    factor = 2
+    ops = 0 
+    current_number = n
+
+    while current_number > 1:
+        while current_number % factor == 0:
+            ops += factor
+            current_number //= factor
+        factor += 1
+    return ops
