c3f71eed3ec0aa7c8d571d67b7b6b096798b0087
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-17 13:46:41 +0200

    fine

--- 0x06-starwars_api/0-starwars_character.js
+++ 0x06-starwars_api/0-starwars_character.js
@@ -4,16 +4,14 @@ const request = require('request');
 const filmId = process.argv[2];
 
 request(`https://swapi-api.alx-tools.com/api/films/${filmId}`, async function (error, response, body) {
-  if (error) console.log(error);
   const chars = JSON.parse(body).characters;
+  
   for (const charLink of chars) {
     await new Promise((resolve, reject) => {
       request(charLink, (error, response, body) => {
-        if (error) console.log(error);
         console.log(JSON.parse(body).name);
         resolve();
       });
     });
   }
 });
-
