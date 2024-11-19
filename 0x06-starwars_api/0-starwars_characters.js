f9493bc845e496cf67f430c3846a5e7ed346d3de
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-19 22:55:21 +0200

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
 const filmId = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${filmId}`, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  }
-  const filmData = JSON.parse(body);
-  const chars = filmData.characters;
-  chars.forEach(charLink => {
-    request(charLink, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      }
-      const charData = JSON.parse(body);
-      console.log(charData.name);
+request(`https://swapi-api.alx-tools.com/api/films/${filmId}`, async function (error, response, body) {
+  if (error) console.error(error);
+  const chars = JSON.parse(body).characters;
+  for (const char of chars) {
+    await new Promise(function (resolve, reject) {
+      request(char, (error, response, body) => {
+        if (error) console.error(error);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
