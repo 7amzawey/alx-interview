153422773db908bd83bbd7e3392f413e0c1e829a
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-21 11:35:43 +0200

    0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,11 +4,10 @@ import copy
 from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> List[List[int]]:
+def rotate_2d_matrix(matrix: List[List[int]]):
     """Rotate the matrix 90 degrees."""
     n = len(matrix)
     mtrx = copy.deepcopy(matrix)
     for i in range(n):
         for j in range(n):
             matrix[i][j] = mtrx[n-j-1][i]
-    return matrix
