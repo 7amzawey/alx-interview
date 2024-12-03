4be395075c35be7689947272a787accad4c9c327
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-03 10:35:13 +0200

    fixing the island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                if j == cols -1 or grid[i][j + 1] == 0:
+                if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
